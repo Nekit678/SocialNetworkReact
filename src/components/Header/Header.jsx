4e28d9f6c94185f42f83b5dd040b83abfdabd1b4
Nekit678 <70006226+Nekit678@users.noreply.github.com>
2022-07-31 19:49:29 +0600

    Lesson 14. CSS-modules.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,8 @@
+import s from './Header.module.css'
+
 function Header() {
     return (
-        <header className="header">
+        <header className={s.header}>
             <img src="https://simg.nicepng.com/png/small/826-8263562_the-react-logo-react-js-logo-svg.png"></img>
         </header>
     );
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,6 +1,8 @@
+import s from './Profile.module.css'
+
 function Profile() {
     return (
-        <div className="content">
+        <div className={s.content}>
         <div>
         <img src="https://media-exp1.licdn.com/dms/image/C4D1BAQGDmALg_8s-Yg/company-background_10000/0/1519799119530?e=2159024400&v=beta&t=4WV9YKR9L3PAEnppWmPPMk5xVnETtWvhZN8NexEzPwM"></img>
         </div>
@@ -12,11 +14,11 @@ function Profile() {
           <div>
             new post
           </div>
-          <div>
-            <div>
+          <div className={s.posts}>
+            <div className={s.item}>
               post1
             </div>
-            <div>
+            <div className={s.item}>
               post2
             </div>
           </div>
