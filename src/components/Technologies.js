1680cea05fec4546232b14247867b6df61515330
Nekit678 <70006226+Nekit678@users.noreply.github.com>
2022-07-31 08:05:01 +0600

    modified:   src/App.css modified:   src/App.js new file:   src/components/Footer.js new file:   src/components/Header.js new file:   src/components/Technologies.js modified:   src/index.css

---
+++ src/components/Technologies.js
@@ -0,0 +1,14 @@
+function Technologies() {
+    return (
+        <div>
+            <ul>
+                <li>dfsdf</li>
+                <li>dfsdf</li>
+                <li>dfsdf</li>
+                <li>dfsdf</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Technologies;
