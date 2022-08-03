2f6e1275cc03f87fbf911844274d12f6735563c4
Nekit678 <70006226+Nekit678@users.noreply.github.com>
2022-08-03 19:10:49 +0600

    Lesson 21. Dialogs

--- src/components/Dialogs/DIalogs.jsx
+++ src/components/Dialogs/DIalogs.jsx
@@ -2,7 +2,30 @@ import s from "./Dialogs.module.css"
 
 function Dialogs(props) {
     return (
-        <div className={s.content}> Dialogs</div>
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog}>
+                    Nikita
+                </div>
+                <div className={s.dialog + ' ' + s.active}>
+                    Milena
+                </div>
+                <div className={s.dialog}>
+                    Homyak
+                </div>
+                <div className={s.dialog}>
+                    Busya
+                </div>
+                <div className={s.dialog}>
+                    Danya
+                </div>
+            </div>
+            <div className={s.messages}>
+               <div className={s.message}>Hi</div>
+               <div className={s.message}>How are you?</div>
+               <div className={s.message}>Lol kekw</div>
+            </div>
+        </div>
     );
 }
 
