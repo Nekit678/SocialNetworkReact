f7977e3be5a27a14fc1dc5ee3d81986caa79578e
Nekit678 <70006226+Nekit678@users.noreply.github.com>
2022-08-04 03:00:47 +0600

    modified:   src/components/Music/Music.jsx

--- src/components/Music/Music.jsx
+++ src/components/Music/Music.jsx
@@ -2,8 +2,10 @@ import s from "./Music.module.css"
 
 function Music(props) {
     return (
-        <div className={s.content}> Music</div>
+        <div
+        className={s.content}>
+            <iframe width="100%" height="520" src="https://music.yandex.ru/iframe/#playlist/rybakovnikita12/3">Слушайте <a href='https://music.yandex.ru/users/rybakovnikita12/playlists/3'>Мне нравится</a> — <a href='https://music.yandex.ru/users/rybakovnikita12'>rybakovnikita12</a> на Яндекс Музыке</iframe>
+        </div>
     );
 }
-
 export default Music;
