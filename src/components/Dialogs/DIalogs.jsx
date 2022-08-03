61c0e0b972df4333273554aaa458f6d2d0d96527
Nekit678 <70006226+Nekit678@users.noreply.github.com>
2022-08-04 00:28:46 +0600

    Lesson 24. UI-BLL

--- src/components/Dialogs/DIalogs.jsx
+++ src/components/Dialogs/DIalogs.jsx
@@ -4,29 +4,46 @@ import { NavLink } from "react-router-dom"
 function Dialog(props) {
     return (
         <div className={s.dialog}>
-            <NavLink to={'/dialogs/'+props.id} className={navData => navData.isActive ? s.active : s.dialog}>{props.name}</NavLink>
+            <NavLink to={'/dialogs/' + props.id} className={navData => navData.isActive ? s.active : s.dialog}>{props.name}</NavLink>
         </div>
     )
 }
 
-function Message(props){
-    return(
+function Message(props) {
+    return (
         <div className={s.message}>{props.message}</div>
     )
 }
 
 function Dialogs(props) {
+
+    let data = [
+        { id: 1, name: "Nikita" },
+        { id: 2, name: "Milena" },
+        { id: 3, name: "Homyak" },
+        { id: 4, name: "Busya" },
+        { id: 5, name: "Danya" }]
+
+    let dataMes = [
+        { id: 1, message: "Hi" },
+        { id: 2, message: "How are you?" },
+        { id: 3, message: "Lol kekw" },
+        { id: 4, message: "Kavagangaa" }]
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <Dialog name="Nikita" id="1"></Dialog>
+                <Dialog name={data[0].name} id={data[0].id}></Dialog>
+                <Dialog name={data[1].name} id={data[1].id}></Dialog>
                 <Dialog name="Milena" id="2"></Dialog>
                 <Dialog name="Homyak" id="3"></Dialog>
                 <Dialog name="Busya" id="4"></Dialog>
                 <Dialog name="Danya" id="5"></Dialog>
             </div>
             <div className={s.messages}>
-                <Message message="Hi"></Message>
+                <Message message={dataMes[0].message}></Message>
+                <Message message={dataMes[1].message}></Message>
+                <Message message={dataMes[2].message}></Message>
                 <Message message="How are you?"></Message>
                 <Message message="Lol kekw"></Message>
             </div>
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,6 +2,11 @@ import s from './MyPosts.module.css'
 import Post from './Post/Post';
 
 function MyPosts() {
+
+  let dataPosts = [
+    { id: 1, message: "Hi, how are you?", likesCount: 12 },
+    { id: 2, message: "It is my first post!", likesCount: 28 }]
+
   return (
     <div className={s.postsBlock}>
       <h3>My posts</h3>
@@ -14,6 +19,8 @@ function MyPosts() {
         </div>
       </div>
       <div className={s.posts}>
+        <Post message={dataPosts[0].message} likesCount={dataPosts[0].likesCount}></Post>
+        <Post message={dataPosts[1].message} likesCount={dataPosts[1].likesCount}></Post>
         <Post message="Hi, how are you?" likesCount='0'></Post>
         <Post message="It is my first post!" likesCount='23'></Post>
 
