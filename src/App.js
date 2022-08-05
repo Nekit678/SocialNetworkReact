dc05be4cc1410a3ec56fb5588b5ece7d6dc79c73
Nekit678 <70006226+Nekit678@users.noreply.github.com>
2022-08-05 22:21:48 +0600

    Lesson 30

--- src/App.js
+++ src/App.js
@@ -11,21 +11,19 @@ import Settings from './components/Settings/Settings';
 
 function App(props) {
   return (
-    <BrowserRouter>
-      <div className="app-wrapper">
-        <Header></Header>
-        <Navbar></Navbar>
-        <div class="app-wrapper-content">
-          <Routes>
-            <Route path='/profile' element={<Profile state = {props.state.profilePage}/>} />
-            <Route path='/dialogs/*' element={<Dialogs state={props.state.dialogsPage}/>} />
-            <Route path='/news' element={<News />} />
-            <Route path='/music' element={<Music />} />
-            <Route path='/settings' element={<Settings />} />
-          </Routes>
-        </div>
+    <div className="app-wrapper">
+      <Header></Header>
+      <Navbar></Navbar>
+      <div class="app-wrapper-content">
+        <Routes>
+          <Route path='/profile' element={<Profile state={props.state.profilePage} />} />
+          <Route path='/dialogs/*' element={<Dialogs state={props.state.dialogsPage} />} />
+          <Route path='/news' element={<News />} />
+          <Route path='/music' element={<Music />} />
+          <Route path='/settings' element={<Settings />} />
+        </Routes>
       </div>
-    </BrowserRouter>
+    </div>
   );
 }
 
