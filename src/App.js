2cd9a020899a4b8edd9269548ea6b952a4b26211
Nekit678 <70006226+Nekit678@users.noreply.github.com>
2022-08-18 23:53:07 +0600

    Lesson 60. WithRouter

--- src/App.js
+++ src/App.js
@@ -16,8 +16,9 @@ function App(props) {
       <Navbar></Navbar>
       <div className="app-wrapper-content">
         <Routes>
-          <Route path='/profile/*' element={<ProfileContainer/>} />
-          <Route path='/dialogs/*' element={<DialogsContainer/>} />
+          <Route path='/profile' element={<ProfileContainer />} />
+          <Route path='/profile/:id' element={<ProfileContainer />} />
+          <Route path='/dialogs/*' element={<DialogsContainer />} />
           <Route path='/news' element={<News />} />
           <Route path='/music' element={<Music />} />
           <Route path='/settings' element={<Settings />} />
--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -3,13 +3,17 @@ import Profile from "./Profile";
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux/es/exports';
 import { setUserProfile } from "../../redux/profile-reducer";
+import { useParams, useSearchParams } from "react-router-dom";
 
 function ProfileContainer(props){
     const dispatch = useDispatch()
     const state = useSelector(state => state.profilePage.userProfile)
 
+    let params = useParams()
+    let userId = (params.id ? params.id:15)
+
     useEffect(() => {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/2`).then(
+        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/${userId}`).then(
             response => {
                 dispatch(setUserProfile(response.data))
             })
