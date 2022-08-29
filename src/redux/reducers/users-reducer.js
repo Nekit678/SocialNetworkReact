392122ef14b7dadd0792d969478b0cb805cccbc6
Nekit678 <70006226+Nekit678@users.noreply.github.com>
2022-08-29 22:51:00 +0600

    Users-reducer refactoring

--- src/redux/reducers/users-reducer.js
+++ src/redux/reducers/users-reducer.js
@@ -2,20 +2,7 @@ import { createSlice } from '@reduxjs/toolkit'
 import { followAPI, usersAPI } from '../../api/api';
 
 let initialState = {
-    users: [
-        // {
-        //     id: 1, photoUrl: "https://i.pinimg.com/236x/2c/60/cb/2c60cb34a209daa60ee0b6c53cd35688.jpg", followed: false, fullName: "Nikita", status: "Just Lena",
-        //     location: { city: "Omsk", country: "Russia" }
-        // },
-        // {
-        //     id: 2, photoUrl: "https://anime-star.ru/wp-content/uploads/2021/12/YArkie-fioletovye-avatarki-anime_08.jpg", followed: true, fullName: "Milena", status: "I am Mommy!",
-        //     location: { city: "Kormilovka", country: "Russia" }
-        // },
-        // {
-        //     id: 3, photoUrl: "https://www.meme-arsenal.com/memes/6eb0717c1db4fd398f312ef918b00eb3.jpg", followed: true, fullName: "Homyak", status: "I like food!",
-        //     location: { city: "Omsk", country: "Russia" }
-        // }
-    ],
+    users: [],
     totalUsersCount: 100,
     pageSize: 10,
     currentPage: 1,
@@ -67,10 +54,10 @@ export function getUsers(currentPage, pageSize) {
 }
 
 
-export function unfollowUser(userId) {
+export function followUnfollowUser(userId, apiMethod) {
     return async (dispatch) => {
-        dispatch(toggleFollowingFetching({ operation: true, id: userId }))
-        let response = await followAPI.unfollow(userId)
+        dispatch(toggleFollowingFetching({ operation: true, id: userId }));
+        let response = await apiMethod(userId)
         if (!response.data.resultCode) {
             dispatch(toggleFollow(userId))
         }
@@ -78,15 +65,15 @@ export function unfollowUser(userId) {
     }
 }
 
+export function unfollowUser(userId) {
+    return async (dispatch) => {
+        dispatch(followUnfollowUser(userId, followAPI.unfollow))
+    }
+}
+
 export function followUser(userId) {
     return async (dispatch) => {
-        dispatch(toggleFollowingFetching({ operation: true, id: userId }))
-        let response = await followAPI.follow(userId)
-
-        if (!response.data.resultCode) {
-            dispatch(toggleFollow(userId))
-        }
-        dispatch(toggleFollowingFetching({ operation: false, id: userId }))
+        dispatch(followUnfollowUser(userId, followAPI.follow))
     }
 }
 
